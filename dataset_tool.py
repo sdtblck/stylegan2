b4a46cd5c8b51659172fb90f9eb5c0cd888addff
sdtblck <46172032+sdtblck@users.noreply.github.com>
2020-06-07 00:21:16 +0100

    Update dataset_tool.py

--- dataset_tool.py
+++ dataset_tool.py
@@ -30,13 +30,13 @@ def error(msg):
 #----------------------------------------------------------------------------
 
 class TFRecordExporter:
-    def __init__(self, tfrecord_dir, expected_images, print_progress=True, progress_interval=10):
+    def __init__(self, tfrecord_dir, expected_images, print_progress=True, progress_interval=10, res_log2=7):
         self.tfrecord_dir       = tfrecord_dir
         self.tfr_prefix         = os.path.join(self.tfrecord_dir, os.path.basename(self.tfrecord_dir))
         self.expected_images    = expected_images
         self.cur_images         = 0
         self.shape              = None
-        self.resolution_log2    = None
+        self.res_log2    = res_log2
         self.tfr_writers        = []
         self.print_progress     = print_progress
         self.progress_interval  = progress_interval
@@ -62,18 +62,73 @@ class TFRecordExporter:
         np.random.RandomState(123).shuffle(order)
         return order
 
+    def add_image_raw(self, encoded_jpg):
+        if self.print_progress and self.cur_images % self.progress_interval == 0:
+            print(
+                "%d / %d\r" % (self.cur_images, self.expected_images),
+                end="",
+                flush=True,
+            )
+        for lod, tfr_writer in enumerate(self.tfr_writers):
+            ex = tf.train.Example(
+                features=tf.train.Features(
+                    feature={
+                        "shape": tf.train.Feature(
+                            int64_list=tf.train.Int64List(value=self.shape)
+                        ),
+                        "img":tf.train.Feature(bytes_list=tf.train.BytesList(value=[encoded_jpg]))
+                    }
+                )
+            )
+            tfr_writer.write(ex.SerializeToString())
+        self.cur_images += 1
+        
+    def create_tfr_writer(self, shape):
+        self.shape = [shape[2], shape[0], shape[1]]
+        assert self.shape[0] in [1, 3]
+        assert self.shape[1] % (2 ** self.res_log2) == 0
+        assert self.shape[2] % (2 ** self.res_log2) == 0
+        tfr_opt = tf.python_io.TFRecordOptions(
+            tf.python_io.TFRecordCompressionType.NONE
+        )
+        tfr_file = self.tfr_prefix + "-r%02d.tfrecords" % (
+                    self.res_log2
+        )
+        self.tfr_writers.append(tf.python_io.TFRecordWriter(tfr_file, tfr_opt))
+
+    def add_image_raw(self, encoded_jpg):
+      if self.print_progress and self.cur_images % self.progress_interval == 0:
+          print(
+              "%d / %d\r" % (self.cur_images, self.expected_images),
+              end="",
+              flush=True,
+          )
+      for lod, tfr_writer in enumerate(self.tfr_writers):
+          ex = tf.train.Example(
+              features=tf.train.Features(
+                  feature={
+                      "shape": tf.train.Feature(
+                          int64_list=tf.train.Int64List(value=self.shape)
+                      ),
+                      "img":tf.train.Feature(bytes_list=tf.train.BytesList(value=[encoded_jpg]))
+                  }
+              )
+          )
+          tfr_writer.write(ex.SerializeToString())
+      self.cur_images += 1
+        
     def add_image(self, img):
         if self.print_progress and self.cur_images % self.progress_interval == 0:
             print('%d / %d\r' % (self.cur_images, self.expected_images), end='', flush=True)
         if self.shape is None:
             self.shape = img.shape
-            self.resolution_log2 = int(np.log2(self.shape[1]))
+            self.res_log2 = int(np.log2(self.shape[1]))
             assert self.shape[0] in [1, 3]
             assert self.shape[1] == self.shape[2]
-            assert self.shape[1] == 2**self.resolution_log2
+            assert self.shape[1] == 2**self.res_log2
             tfr_opt = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.NONE)
-            for lod in range(self.resolution_log2 - 1):
-                tfr_file = self.tfr_prefix + '-r%02d.tfrecords' % (self.resolution_log2 - lod)
+            for lod in range(self.res_log2 - 1):
+                tfr_file = self.tfr_prefix + '-r%02d.tfrecords' % (self.res_log2 - lod)
                 self.tfr_writers.append(tf.python_io.TFRecordWriter(tfr_file, tfr_opt))
         assert img.shape == self.shape
         for lod, tfr_writer in enumerate(self.tfr_writers):
@@ -557,7 +612,7 @@ def create_from_images_raw(tfrecord_dir, image_dir, shuffle, res_log2=7, resize=
     if resize is not None:
         out_dir = os.path.abspath(os.path.join(image_dir, os.pardir))
         try:
-            {out_dir}/resized_{resize}/
+            os.mkdir(f"{out_dir}/resized_{resize}/")
         except:
             pass
         print(f"Resizing {len(image_filenames)} images to {resize} x {resize}")
