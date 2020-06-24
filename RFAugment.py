1350997bf22a63b611f689e21d3e48bb232b6866
sdtblck <sdtblck@gmail.com>
2020-06-24 13:59:39 +0100

    added zhao et al augs

--- RFAugment.py
+++ RFAugment.py
@@ -9,6 +9,7 @@
 
 import tensorflow as tf
 import os
+import math
 
 def augment(batch, policy='', channels_first=True, mode='gpu'):
     if mode == 'gpu':
@@ -640,8 +641,8 @@ def apply_zoom_in_tpu(x, alpha=alpha_default):
     x.set_shape(x.shape)
     h = int(x.shape[0])
     # range = img.size*(1-alpha) -> img.size
-    min_val = int(h * (1 - alpha))
-    choice = tf.random_uniform([], min_val, x.shape[0], tf.int32)
+    min_val = 64 #TODO: shouldn't be hardcoded
+    choice = tf.random_uniform([], min_val, h, tf.int32)
     for crop_size in range(min_val, x.shape[0]):
         x = tf.cond(tf.reduce_all(tf.equal(choice, tf.constant(crop_size))), lambda: zoom_in_tpu(x, crop_size), lambda: tf.identity(x))
     return x
@@ -890,6 +891,82 @@ def xy_translate_custom(img, y, x, out_shape=None):
     raise Exception('Y value must be < height of image - 1')
   return tf.slice(padded_img, [y, x, 0], img.shape)
 
+
+def get_mat(rotation, shear, height_zoom, width_zoom, height_shift, width_shift):
+    # returns 3x3 transformmatrix which transforms indicies
+
+    # CONVERT DEGREES TO RADIANS
+    rotation = math.pi * rotation / 180.
+    shear = math.pi * shear / 180.
+
+    # ROTATION MATRIX
+    c1 = tf.math.cos(rotation)
+    s1 = tf.math.sin(rotation)
+    one = tf.constant([1], dtype='float32')
+    zero = tf.constant([0], dtype='float32')
+    rotation_matrix = tf.reshape(tf.concat([c1, s1, zero, -s1, c1, zero, zero, zero, one], axis=0), [3, 3])
+    print(rotation_matrix.shape)
+
+    # SHEAR MATRIX
+    c2 = tf.math.cos(shear)
+    s2 = tf.math.sin(shear)
+    shear_matrix = tf.reshape(tf.concat([one, s2, zero, zero, c2, zero, zero, zero, one], axis=0), [3, 3])
+    print(shear_matrix.shape)
+
+    # ZOOM MATRIX
+    zoom_matrix = tf.reshape(
+        tf.concat([one / height_zoom, zero, zero, zero, one / width_zoom, zero, zero, zero, one], axis=0), [3, 3])
+    print(zoom_matrix.shape)
+
+    # SHIFT MATRIX
+    shift_matrix = tf.reshape(tf.concat([one, zero, height_shift, zero, one, width_shift, zero, zero, one], axis=0),
+                              [3, 3])
+    print(shift_matrix.shape)
+
+    return tf.tensordot(tf.tensordot(rotation_matrix, shear_matrix, 1), tf.tensordot(zoom_matrix, shift_matrix, 1), 1)
+
+def experimental_transform(image):
+    # input image - is one image of size [dim,dim,3] not a batch of [b,dim,dim,3]
+    # output - image randomly rotated, sheared, zoomed, and shifted
+    DIM = 128
+    XDIM = DIM % 2  # fix for size 331
+
+    # rot = 15. * tf.random.normal([1], dtype='float32')
+    # shr = 5. * tf.random.normal([1], dtype='float32')
+    rot = tf.constant([0], dtype='float32')
+    shr = tf.constant([0], dtype='float32')
+    h_zoom = 1.0 + tf.random.normal([1], dtype='float32') / 10.
+    w_zoom = h_zoom
+    # w_zoom = 1.0 + tf.random.normal([1], dtype='float32') / 10.
+    h_shift = 16. * tf.random.normal([1], dtype='float32')
+    w_shift = 16. * tf.random.normal([1], dtype='float32')
+
+    # GET TRANSFORMATION MATRIX
+    m = get_mat(rot, shr, h_zoom, w_zoom, h_shift, w_shift)
+
+    # LIST DESTINATION PIXEL INDICES
+    x = tf.repeat(tf.range(DIM // 2, -DIM // 2, -1), DIM)
+    y = tf.tile(tf.range(-DIM // 2, DIM // 2), [DIM])
+    z = tf.ones([DIM * DIM], dtype='int32')
+    idx = tf.stack([x, y, z])
+
+    # ROTATE DESTINATION PIXELS ONTO ORIGIN PIXELS
+    # idx2 = K.dot(m, tf.cast(idx, dtype='float32'))
+    # idx2 = K.cast(idx2, dtype='int32')
+    # idx2 = K.clip(idx2, -DIM // 2 + XDIM + 1, DIM // 2)
+    idx2 = tf.tensordot(m, tf.cast(idx, dtype='float32'), 1)
+    idx2 = tf.cast(idx2, dtype='int32')
+    idx2 = tf.clip_by_value(idx2, -DIM // 2 + XDIM + 1, DIM // 2)
+
+
+
+    # FIND ORIGIN PIXEL VALUES
+    idx3 = tf.stack([DIM // 2 - idx2[0,], DIM // 2 - 1 + idx2[1,]])
+    d = tf.gather_nd(image, tf.transpose(idx3))
+
+    return tf.reshape(d, [DIM, DIM, 3])
+
+
 def apply_random_aug_tpu(x):
     """
     Apply random spatial augmentation to TF image.
@@ -907,6 +984,7 @@ def apply_random_aug_tpu(x):
     x = tf.cond(tf.reduce_all(tf.equal(choice, tf.constant(3))), lambda: apply_random_cutout_tpu(x), lambda: tf.identity(x))
     return x
 
+
 SINGLE_IMG_FNS_TPU = {
     'zoomin': [apply_zoom_in_tpu],
     'zoomout': [apply_zoom_out_tpu],
@@ -915,5 +993,6 @@ SINGLE_IMG_FNS_TPU = {
     'xytrans': [apply_XY_translate_tpu],
     'cutout': [apply_random_cutout_tpu],
     'random': [apply_random_aug_tpu],
-    'customxy': [apply_custom_xy_translate]
+    'customxy': [apply_custom_xy_translate],
+    'experimentaltrans': [experimental_transform]
 }
